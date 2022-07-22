1d0f5cac217ffc5d7347139e11f47ec192175b43
marksongg <marksongg@163.com>
2022-07-22 10:55:14 +0800

    检索组件，轮播组件，页脚组件新规追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import ApplicationComponent from './components/application/application'
-import NavbarComponent from './components/navbar/navbar'
+import ApplicationComponent from './components/application/application';
+import NavbarComponent from './components/navbar/navbar';
+import FooterComponent from './components/footer/footer';
+import SearchComponent from './components/search/search';
+import CarouselComponent from './components/carousel/carousel';
 
 @NgModule({
   declarations: [
     AppComponent, 
     ApplicationComponent, // ★重要，这里必须要追加，否则*ngfor等命令不可以使用
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +27,10 @@ import NavbarComponent from './components/navbar/navbar'
   bootstrap: [
     AppComponent, 
     ApplicationComponent, // ★重要，这里必须要追加，否则*ngfor等命令不可以使用
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent
   ]
 })
 export class AppModule { }
